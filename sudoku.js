8513d7a1df907a751013fa8b12db81b85671ae95
loddit <loddit@gmail.com>
2012-06-22 14:20:16 +0800

    interface and player system

--- sudoku.js
+++ sudoku.js
@@ -1,17 +1,32 @@
 Grids = new Meteor.Collection("grids");
-Users = new Meteor.Collection("users");
+Players = new Meteor.Collection("players");
 var numbers = ['','1','2','3','4','5','6','7','8','9']
 
 if (Meteor.is_client) {
-  var random_color = "#" + Math.floor(Math.random()*10) + Math.floor(Math.random()*10) + Math.floor(Math.random()*10)
-  current_user_hash = Users.insert({name:'test',color: random_color,score: 0});
-  current_user = Users.findOne(current_user_hash);
-  Template.map.grids = function(){
+  
+  Template.form.events = {
+    'submit': function(event){
+      event.preventDefault();
+      var random_color = "#" + Math.floor(Math.random()*10) + Math.floor(Math.random()*10) + Math.floor(Math.random()*10)
+      current_player_hash = Players.insert({name: $('#name').val(),color: random_color,score: 0},function(){$(event.target).html("Game Started !")});
+      current_player = Players.findOne(current_player_hash);
+    }
+  }
+
+  Template.sudoku.grids = function(){
     return Grids.find({})
   }
 
   Template.grid.is_error = function(){
     return this.error === true;
+  }
+
+  Template.player.is_current = function(){
+    if (typeof(current_player_hash) == 'undefined'){
+       return false
+    }else{
+        return this._id == current_player_hash;
+    }
   }
 
   Template.grid.events = {
@@ -27,28 +42,36 @@ if (Meteor.is_client) {
       }else{
         var error = false
       }
-      if (number == ''){ var user_hash = ''}else{ var user_hash = current_user_hash}
+      if (number == ''){ var player_hash = ''}else{ var player_hash = current_player_hash}
       Grids.update(
         {row: parseInt(grid.attr('data-row')),col: parseInt(grid.attr('data-col'))},
-        {$set: { number: number, error: error, color: current_user.color, user: user_hash}},
+        {$set: { number: number, error: error, color: current_player.color, player: player_hash}},
         {multi: false},
         function(){
         }
       )
-      var score = Grids.find({user:current_user_hash, error: false }).count()
-      Users.update(current_user_hash, {$set: {score: score}})
+      var score = Grids.find({player:current_player_hash, error: false }).count()
+      Players.update(current_player_hash, {$set: {score: score}})
+    },
+    'click': function(event){
+      if ( typeof(current_player) == 'undefined'){
+        alert("Join game first :)");
+        event.target.blur();
+        event.preventDefault();
+      }
     }
+
   }
 
-  Template.rank.users = function(){
-    return Users.find({})
+  Template.rank.players = function(){
+    return Players.find({})
   }
 }
 
 if (Meteor.is_server) {
   Meteor.startup(function () {
     Grids.remove({});
-    Users.remove({});
+    Players.remove({});
     var game = [
             [2, ,5, , ,7, , ,6,],
             [4, , ,9,6, , ,2, ,],
@@ -69,7 +92,7 @@ if (Meteor.is_server) {
           number = ''
           disabled= ''
         }
-        Grids.insert({number: number, disabled: disabled ,row: row, col: col,block: (Math.floor(col/3) + 3*Math.floor(row/3)),user: 'system',error: false});
+        Grids.insert({number: number, disabled: disabled ,row: row, col: col,block: (Math.floor(col/3) + 3*Math.floor(row/3)),player: 'system',error: false});
       };
     });
   });
