13e5c819b977e644be90c8a48bde7f4ff291fe74
loddit <loddit@gmail.com>
2012-06-23 21:22:17 +0800

    add chatroom

--- sudoku.js
+++ sudoku.js
@@ -1,83 +1,74 @@
 Grids = new Meteor.Collection("grids");
 Players = new Meteor.Collection("players");
+Messages = new Meteor.Collection("message");
+
 var numbers = ['','1','2','3','4','5','6','7','8','9']
-
-  function GameInit(){
-    Grids.remove({});
-    Players.remove({});
-    var games = [
-          [
-            [2, ,5, , ,7, , ,6,],
-            [4, , ,9,6, , ,2, ,],
-            [ , , , ,8, , ,4,5,],
-            [9,8, , ,7,4, , , ,],
-            [5,7, ,8, ,2, ,6,9,],
-            [ , , ,6,3, , ,5,7,],
-            [7,5, , ,2, , , , ,],
-            [ ,6, , ,5,1, , ,2,],
-            [3, , ,4, , ,5, ,8,]
-          ],
-          [
-            [8, , ,4, ,6, , ,7,],
-            [ , , , , , ,4, , ,],
-            [ ,1, , , , ,6,5, ,],
-            [5, ,9, ,3, ,7,8, ,],
-            [ , , , ,7, , , , ,],
-            [ ,4,8, ,2, ,1, ,3,],
-            [ ,5,2, , , , ,9, ,],
-            [ , ,1, , , , , , ,],
-            [3, , ,9, ,2, , ,5,]
-          ],
-          [
-            [ ,9, ,1, , ,3, , ,],
-            [ ,1, , ,6, , ,2,4,],
-            [7, , ,3,8, , , , ,],
-            [ , , , , , ,4, ,6,],
-            [ ,8,3, , , ,1,9, ,],
-            [2, ,7, , , , , , ,],
-            [ , , , ,9,3, , ,5,],
-            [6,7, , ,2, , ,8, ,],
-            [ , ,9, , ,4, ,6, ,]
-          ],
-        ]
-
-    var answer = [
-          [4,8,5,3,1,9,7,2,6,],
-          [6,1,3,8,2,7,5,4,9,],
-          [7,9,2,6,5,4,8,3,1,],
-          [2,5,8,9,4,3,2,6,7,],
-          [1,3,6,5,7,2,9,8,4,],
-          [7,9,4,1,6,5,3,5,2,],
-          [8,6,7,2,9,5,4,1,3,],
-          [3,4,1,7,8,6,2,9,5,],
-          [5,2,9,4,3,1,6,7,8,]
-        ]
-    var game  = games[Math.floor( Math.random()*games.length)]
-    _.each(game,function(item,row){
-      for(var col = 0; col < item.length; col++){
-        var number = item[col]
-        var disabled="disabled"
-        if(number == undefined ){
-          number = ''
-          disabled= ''
+var games = [
+      [
+        [2, ,5, , ,7, , ,6,],
+        [4, , ,9,6, , ,2, ,],
+        [ , , , ,8, , ,4,5,],
+        [9,8, , ,7,4, , , ,],
+        [5,7, ,8, ,2, ,6,9,],
+        [ , , ,6,3, , ,5,7,],
+        [7,5, , ,2, , , , ,],
+        [ ,6, , ,5,1, , ,2,],
+        [3, , ,4, , ,5, ,8,]
+      ],
+      [
+        [8, , ,4, ,6, , ,7,],
+        [ , , , , , ,4, , ,],
+        [ ,1, , , , ,6,5, ,],
+        [5, ,9, ,3, ,7,8, ,],
+        [ , , , ,7, , , , ,],
+        [ ,4,8, ,2, ,1, ,3,],
+        [ ,5,2, , , , ,9, ,],
+        [ , ,1, , , , , , ,],
+        [3, , ,9, ,2, , ,5,]
+      ],
+      [
+        [ ,9, ,1, , ,3, , ,],
+        [ ,1, , ,6, , ,2,4,],
+        [7, , ,3,8, , , , ,],
+        [ , , , , , ,4, ,6,],
+        [ ,8,3, , , ,1,9, ,],
+        [2, ,7, , , , , , ,],
+        [ , , , ,9,3, , ,5,],
+        [6,7, , ,2, , ,8, ,],
+        [ , ,9, , ,4, ,6, ,]
+      ],
+    ]
+ 
+function GameInit(){
+  Grids.remove({});
+  Players.remove({});
+  Messages.remove({});
+  var game  = games[Math.floor( Math.random()*games.length)]
+  _.each(game,function(item,row){
+    for(var col = 0; col < item.length; col++){
+      var number = item[col]
+      var disabled="disabled"
+      if(number == undefined ){
+        number = ''
+        disabled= ''
+      }
+      Grids.insert(
+        {
+          number: number, 
+          disabled: disabled ,
+          row: row,
+          col: col,
+          block: (Math.floor(col/3) + 3*Math.floor(row/3)),
+          player: 'system',
+          error: false,
+          color: 'black'
+        },
+        function(){
         }
-        Grids.insert(
-          {
-            number: number, 
-            disabled: disabled ,
-            row: row,
-            col: col,
-            block: (Math.floor(col/3) + 3*Math.floor(row/3)),
-            player: 'system',
-            error: false,
-            color: 'black'
-          },
-          function(){
-          }
-        );
-      };
-    });
-  }
+      );
+    };
+  });
+}
 
 
 if (Meteor.is_client) {
@@ -93,10 +84,11 @@ if (Meteor.is_client) {
         }
       );
       $(event.target).replaceWith(Meteor.ui.render(Template.form))
+      $("#say").replaceWith(Meteor.ui.render(Template.say))
     }
   }
 
-  Template.form.has_current_player = function(){
+  Template.say.has_current_player = Template.form.has_current_player = function(){
     return typeof(current_player_hash) != 'undefined' && Players.find()
   }
 
@@ -173,11 +165,22 @@ if (Meteor.is_client) {
       current_player_hash = current_player = undefined
       $(Template.form).replaceWith(Meteor.ui.render(Template.form))
       $(event.target).parent().remove()
-      
+    }
+  }
+
+  Template.chatroom.messages = function(){
+    return Messages.find({},{sort:{time: -1}})
+  }
+
+  Template.say.events = {
+    "submit": function(event){
+      event.preventDefault();
+      var say = $(event.target)
+      Messages.insert({content: say.find('input#content').val(), player: current_player,time: new Date()});
+      $(event.target).replaceWith(Meteor.ui.render(Template.say))
     }
   }
 }
-
 
 
 if (Meteor.is_server) {
